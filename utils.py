aefb1721c19489b440c78fe03e1646898e045f77
Devanshi Modak <devanshi1630@gmail.com>
2025-12-23 11:10:48 +0530

    Fix Streamlit Cloud dependency issue

--- utils.py
+++ utils.py
@@ -1,11 +1,10 @@
 import requests
 import os
-from dotenv import load_dotenv
 
 # -------------------------------------------------
 # Load Environment Variables
 # -------------------------------------------------
-load_dotenv()
+
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
 BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
